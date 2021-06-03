be6a14a7a7dcf66c777a0a16e5f1e358833b6e50
samucorea <samuel.alejandro100@hotmail.com>
2021-06-03 16:26:23 -0400

    Remove duplicates(day 2) done)

--- main.js
+++ main.js
@@ -1,43 +1,11 @@
-function bubbleSort(arr) {
-    let swapped;
-
-    do {
-        swapped = false
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp
-
-                swapped = true
-            }
-
-        }
-    } while (swapped)
-}
-
-
 function removeDuplicates(arr) {
-    let arrNoDup = []
 
-    arr.forEach(element => {
-        if (!arrNoDup.includes(element)) {
-            arrNoDup.push(element)
-        }
-    })
+    let uniqueArr = arr.filter((element, index) => arr.indexOf(element) === index)
 
-    return arrNoDup
+    return uniqueArr
 }
 
-function sortAndRemoveDuplicates(arr) {
-    const arrNoDup = removeDuplicates(arr)
 
-    bubbleSort(arrNoDup)
+const arr = [5, 5, 5, 3, 2, 2, 3, 1, 1, 1]
 
-    return arrNoDup
-}
-const arr = [2, -4, 19, 10, 5, 7, 2, -4]
-
-const arrNoDupSorted = sortAndRemoveDuplicates(arr)
-
-console.log(arrNoDupSorted)
+console.log(removeDuplicates(arr))
