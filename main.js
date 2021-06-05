0236bba854ad99c04afa9a848684ca5d57a95253
samucorea <samuel.alejandro100@hotmail.com>
2021-06-04 21:39:53 -0400

    removeDuplicates done(kata day 3)

--- main.js
+++ main.js
@@ -1,39 +1,6 @@
-function removeDuplicates(arr) {
 
-    let uniqueArr = arr.filter((element, index) => arr.indexOf(element) === index)
+const removeDuplicates = arr => [...new Set(arr)]
 
-    return uniqueArr
-}
+let arr = [3, 3, 3, 3, 1, 1, 1, 4, 4, 4, 8, 8]
 
-function insertionSort(arr) {
-    let holePosition, key
-
-    for (let i = 1; i < arr.length; i++) {
-        key = arr[i]
-        holePosition = i
-
-        while (holePosition > 0 && arr[holePosition - 1] > key) {
-            arr[holePosition] = arr[holePosition - 1]
-            holePosition -= 1
-        }
-
-        arr[holePosition] = key
-
-    }
-
-}
-
-function removeDuplicatesAndSort(arr) {
-    let arrNoDup = removeDuplicates(arr)
-
-    insertionSort(arrNoDup)
-
-    return arrNoDup
-}
-
-const arr = [5, 5, 5, 3, 2, 2, 3, 1, 1, 1]
-
-let arrOrg = removeDuplicatesAndSort(arr)
-
-
-console.log(arrOrg)
+console.log(removeDuplicates(arr))
